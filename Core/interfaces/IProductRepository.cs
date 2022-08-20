47f65f28202ef357b7ecacf697aac2f6dd5c4156
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2022-08-20 08:40:05 +0700

    Add Brands, Types

--- Core/interfaces/IProductRepository.cs
+++ Core/interfaces/IProductRepository.cs
@@ -7,7 +7,9 @@ namespace Core.interfaces
     public interface IProductRepository
     {
         Task<Product> GetProductByIdAsync(int id);
-        Task<IReadOnlyList<Product>> GetProductAsync();
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
         
     }
 }
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -16,14 +16,25 @@ namespace Infrastructure.Data
             _context = context;
         }
 
-        public async Task<IReadOnlyList<Product>> GetProductAsync()
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
             return await _context.Products.ToListAsync();
         }
 
+     
         public async Task<Product> GetProductByIdAsync(int id)
         {
             return await _context.Products.FindAsync(id);
+        }
+           public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
         }
     }
 }
