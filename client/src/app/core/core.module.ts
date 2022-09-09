1e940320c111461ee6b150c0d84b80e6bb9939aa
NguyenVu <vunq20@wru.vn>
2022-09-09 09:48:45 +0700

    SS12E02 Adding a section header

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -7,10 +7,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -20,6 +21,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
       preventDuplicates: true
     })
   ],
-  exports :[NavBarComponent]
+  exports :[
+    NavBarComponent,
+    SectionHeaderComponent
+  ]
 })
 export class CoreModule { }
