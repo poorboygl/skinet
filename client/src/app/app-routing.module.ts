5e92f1722f25b09d366c4c28528b8e93da970d6f
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2022-09-15 14:50:00 +0700

    fix Error Breadcrumb

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
@@ -10,16 +11,13 @@ import { ShopComponent } from './shop/shop.component';
 const routes: Routes = [
   {path:'', component: HomeComponent,
     data:{
-      breakcrumb:{
-        label: 'app home',
-        info: 'home'
-      }
+      breadcrumb:'Home'
     }
   },
-  {path:'test-error', component: TestErrorComponent, data:{breakcrumb: 'Test Errors'}},
-  {path:'server-error', component: ServerErrorComponent, data:{breakcrumb: 'Server Errors'}},
-  {path:'not-found', component: NotFoundComponent, data:{breakcrumb: 'Not Found'}},
-  {path: 'shop', loadChildren:()=>import('./shop/shop.module').then(mod=>mod.ShopModule),data:{breakcrumb: 'Shop'}},
+  {path:'test-error', component: TestErrorComponent, data:{breadcrumb: 'Test Errors'}},
+  {path:'server-error', component: ServerErrorComponent, data:{breadcrumb: 'Server Errors'}},
+  {path:'not-found', component: NotFoundComponent, data:{breadcrumb: 'Not Found'}},
+  {path: 'shop', loadChildren:()=>import('./shop/shop.module').then(mod=>mod.ShopModule),data:{breadcrumb: 'Shop'}},
   {path:'**',redirectTo:'not-found', pathMatch:'full'}
 
 ];
