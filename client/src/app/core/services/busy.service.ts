947c941de40a80eeb9f21cfbd04790792e17e20b
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2022-09-17 15:28:00 +0700

    SS12E10

--- client/src/app/core/services/busy.service.ts
+++ client/src/app/core/services/busy.service.ts
@@ -10,8 +10,8 @@ export class BusyService {
   constructor(private spinnerService: NgxSpinnerService) { }
   busy(){
     this.busyRequestCount++;
-    this.spinnerService.show(undefined,{
-      type: 'pacman',
+    this.spinnerService.show(undefined, {
+      type: 'timer',
       bdColor: 'rgba(255,255,255,0.7)',
       color: '#333333',
       fullScreen: false
