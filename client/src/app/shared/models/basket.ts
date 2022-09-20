a853849a469bef1b0d447ea6076c0292fe98d71c
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2022-09-20 22:18:06 +0700

    SS14E03

---
+++ client/src/app/shared/models/basket.ts
@@ -0,0 +1,23 @@
+import {v4 as uuid4} from 'uuid';
+
+export interface IBasket {
+    id: string;
+    items: IBasketItem[];
+}
+
+export interface IBasketItem {
+    id: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
+}
+
+export class Basket implements IBasket{
+    id = uuid4();
+    items: IBasketItem[];
+
+
+}
