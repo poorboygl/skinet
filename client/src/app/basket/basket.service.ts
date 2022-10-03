79a26a168a91f8fff2bf5e68aab921304a2f7dfc
NguyenVu <vunq20@wru.vn>
2022-10-03 16:13:18 +0700

    SS14E12

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -51,6 +51,45 @@ export class BasketService {
     basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
     this.setBasket(basket);
   }
+  incrementItemQuatity(item: IBasketItem){
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex(x => x.id === item.id);
+    basket.items[foundItemIndex].quantity++;
+    this.setBasket(basket);
+  }
+  decrementItemQuatity(item: IBasketItem){
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex(x => x.id === item.id);
+    if(basket.items[foundItemIndex].quantity >1){
+      basket.items[foundItemIndex].quantity--;
+    }
+    else{
+      this.removeItemFromBasket(item);
+    }
+  }
+
+  removeItemFromBasket(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    if(basket.items.some(x =>x.id === item.id)){
+      basket.items = basket.items.filter(i => i.id !== item.id);
+      if (basket.items.length>0){
+        this.setBasket(basket);
+      }else{
+        this.deleteBasket(basket);
+      }
+    }
+  }
+  deleteBasket(basket: IBasket) {
+    return this.http.delete(this.baseUrl+'basket?id='+ basket.id).subscribe({
+      next: () =>{
+        this.basketSource.next(null!);
+        this.basketTotalSource.next(null!);
+        localStorage.removeItem('basket_id');
+      },
+      error: (e) => console.log(e)
+      // ,complete: () => console.info('complete') 
+    });
+  }
 
   private calcuateTotals(){
     const basket = this.getCurrentBasketValue();
