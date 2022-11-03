2da63ee2b0866fa0f34510aa76e369927de63ec6
NguyenVu <vunq20@wru.vn>
2022-11-03 11:38:04 +0700

    SS17E09

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -16,7 +16,23 @@ export class AccountService {
 
   constructor(private http: HttpClient, private router: Router) {}
 
-    login(values: any){
+  getCurrentUserValue(){
+    return this.currentUserSource.value;
+  }
+
+  loadCurrentUser(token: string){
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization',`Bearer ${token}`);
+    return this.http.get<IUser>(this.baseUrl+ 'account', {headers}).pipe(
+      map((user: IUser) =>{
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+      })
+    )
+
+  }
+
+  login(values: any){
         return this.http.post<IUser>(this.baseUrl+ 'account/login', values).pipe(
           map((user: IUser) =>{
             if (user){
@@ -27,7 +43,7 @@ export class AccountService {
         );
    }
 
-   register(values: any){
+  register(values: any){
       return this.http.post<IUser>(this.baseUrl+ 'account/register', values).pipe(
         map((user: IUser) =>{
           if(user){
@@ -37,13 +53,13 @@ export class AccountService {
       );
    }
 
-   logout(){
+  logout(){
     localStorage.removeItem('token');
     this.currentUserSource.next(null!);
     this.router.navigateByUrl('/');
    }
 
-   checkEmailExists(email: string){
+  checkEmailExists(email: string){
       return this.http.get(this.baseUrl+'/account/emailexists?email='+ email);
    }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,14 +10,32 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit  {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService){}
+  constructor(private basketService: BasketService, private accountService: AccountService){}
   ngOnInit(): void {
-      const basketId = localStorage.getItem('basket_id');
-      if(basketId){
-        this.basketService.getBasket(basketId).subscribe({
-          next: () => console.log('Initialised basket'),
-          error: (e) => console.log(e), 
-        })
-      }
+    this.loadBasket();
+    this.loadCurrentUser();
+    
   }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+    if(token){
+      this.accountService.loadCurrentUser(token).subscribe({
+        next: () => console.log('Loaded User'),
+        error: (e) => console.log(e)
+        // ,complete: () => console.info('complete') 
+      });
+    }
+  }
+
+  loadBasket(){
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId){
+      this.basketService.getBasket(basketId).subscribe({
+        next: () => console.log('Initialised basket'),
+        error: (e) => console.log(e), 
+      });
+    }
+  }
+
 }
