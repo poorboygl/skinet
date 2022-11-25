96f941d169316d9d059a7e4be8a4408f67a902a0
NguyenVu <vunq20@wru.vn>
2022-11-25 13:20:23 +0700

    SS18E5

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -32,6 +33,28 @@ namespace Infrastructure.Data
                     foreach (var item in types)
                     {
                         context.ProductTypes.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+                if(!context.Products.Any())
+                {
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                    foreach (var item in products)
+                    {
+                        context.Products.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+                if(!context.DeliveryMethods.Any())
+                {
+                    var dmData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                    var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
+                    foreach (var item in methods)
+                    {
+                        context.DeliveryMethods.Add(item);
                     }
                     await context.SaveChangesAsync();
                 }
