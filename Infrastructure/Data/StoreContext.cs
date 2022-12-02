c910a1ae77eee63a57954d4076e141c5e4713dc4
NguyenVu <vunq20@wru.vn>
2022-12-02 15:27:12 +0700

    SS18E18

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using Core.Entities;
 using Core.Entities.OrderAggregate;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data
 {
@@ -30,11 +31,19 @@ namespace Infrastructure.Data
                 {
                     var properties = entityType.ClrType.GetProperties()
                             .Where(p=>p.PropertyType == typeof(decimal));
+                    var dateTimeProperties = entityType.ClrType.GetProperties()
+                            .Where(p => p.PropertyType == typeof(DateTimeOffset));
 
                     foreach (var property in properties )
                     {
                         modelBuilder.Entity(entityType.Name).Property(property.Name)
                         .HasConversion<double>();
+                    }
+
+                    foreach (var property in dateTimeProperties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                        .HasConversion(new DateTimeOffsetToBinaryConverter());
                     }
                 }
             }
